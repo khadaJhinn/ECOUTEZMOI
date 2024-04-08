6edb6f65a70aeb49bed196df6d06b08efe073524
Calibrea <163871680+Calibrea@users.noreply.github.com>
2024-04-08 17:00:43 +0200

    Create test_localstorage.js

---
+++ www/test_localstorage.js
@@ -0,0 +1,8 @@
+var pseudo= document.getElementById("pseudo");
+localStorage.setItem("pseudo",pseudo.value);
+
+var 1topic= document.getElementById("1topic");
+localStorage.setItem("1topic", 1topic.value);
+
+var topic= document.getElementById("topic");
+localStorage.setItem("topic", topic.value);
