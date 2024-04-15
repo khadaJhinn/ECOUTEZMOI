3434a3c1230467f621c279c113dfd44261cd0b6b
Crobus <wg.whooping298@passinbox.com>
2024-04-15 17:16:02 +0200

    Update test_localstorage.js

--- www/test_localstorage.js
+++ www/test_localstorage.js
@@ -1,25 +1,4 @@
-// fetch('bdd.json')
-//   .then(response => response.json()) // Convertir la réponse en JSON
-//   .then(data => {
-//     // Accéder aux données
-//     console.log(data.squadName); // Affichera 'Super hero squad'
-//     console.log(data.homeTown); // Affichera 'Metro City'
-//     console.log(data.formed); // Affichera 2016
-//     console.log(data.secretBase); // Affichera 'Super tower'
-//     console.log(data.active); // Affichera true
-
-//     // Accéder aux membres
-//     data.members.forEach(member => {
-//       console.log("Membre:", member.name);
-//       console.log("Âge:", member.age);
-//       console.log("Identité secrète:", member.secretIdentity);
-//       console.log("Pouvoirs:", member.powers);
-//     });
-//   })
-//   .catch(error => console.error('Une erreur s\'est produite:', error));
-
-
-var data=  {
+var data = {
     "squadName": "Super hero squad",
     "homeTown": "Metro City",
     "formed": 2016,
@@ -57,4 +36,4 @@ var data=  {
     ]
   }
 
-  
+  document.getElementById("com").textContent= data;
